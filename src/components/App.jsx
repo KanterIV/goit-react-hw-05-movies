f40d05d8fc57606a9b82f826fcb90b1dcde17d12
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-10-15 19:28:28 +0200

    Added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from './Header/Header';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
+import { AppWrapper } from './Wrapper/Wrapper.styled';
 
 export const App = () => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <AppWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AppWrapper>
+    </>
+  );
 };
---
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchMovies = () => {
+  return <div>SearchMovies</div>;
+};
+
+export default SearchMovies;
